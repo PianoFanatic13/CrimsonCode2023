1a5a1bb39424a0ac054db7c47e9fb0f4306571f3
Peter Liu <124539148+liu-petter@users.noreply.github.com>
2023-02-18 15:58:57 -0800

    updated newtask

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-function getTime()
+function newTask()
 {
-    var inputTime = document.getElementById("getTime").value;
-    document.getElementById("demo").innerHTML = inputTime;
+    var inputTime = document.getElementById("newTask-Time").value;
+    var inputCategory = document.getElementById("newTask-Category").value;
+    var inputName = document.getElementById("newTask-Name").value;
+    console.log(inputTime);
+    console.log(inputCategory);
+    console.log(inputName);
 }
