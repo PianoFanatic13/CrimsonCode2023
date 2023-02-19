51d0b56ffcf2fb6aace25d07657a7d409e0f27d0
PianoFanatic13 <abdulraqib0224@gmail.com>
2023-02-18 19:43:25 -0800

    index.js changed for good todo-List functionality

--- index.js
+++ index.js
@@ -1,58 +1,55 @@
-window.addEventListener('load', () => {
+const tasklist = document.querySelector("#tasks")
+const inputName = document.querySelector("#newTask-Name");
+let numTasks = 0;
 
-    const tasklist = document.querySelector("#tasks")
-    const inputName = document.querySelector("#newTask-Name");
-    let numTasks = 0;
+document.querySelector("#newTask-Form").addEventListener("submit", (event) => {
+    event.preventDefault()
+    const name = inputName.value;
     
-    document.querySelector("#newTask-Form").addEventListener("submit", (event) => {
-        event.preventDefault()
-        const name = inputName.value;
+    if (!name){
+        alert("Please fill out a task!");
+    }
+
+    else{
+        const taskContent = document.createElement('div');
+
+        const textBox = document.createElement('input');
+        textBox.classList.add('text');
+        textBox.id = numTasks;
+        textBox.type = "text";
+        textBox.value = name;
+        textBox.setAttribute('readonly', 'readonly');
+
+        const editContent = document.createElement('button');
+        editContent.classList.add('edit');
+        editContent.innerText = '+';
+
+        const delContent = document.createElement('button');
+        delContent.classList.add('delete');
+        delContent.innerText = '-';
+
         
-        if (!name){
-            alert("Please fill out a task!");
-        }
+        taskContent.append(textBox);
+        taskContent.append(editContent);
+        taskContent.append(delContent);
 
-        else{
-            const taskContent = document.createElement('div');
+        tasklist.appendChild(taskContent);
+        
+        numTasks++;
+        
+        editContent.addEventListener('click', (event) =>{
+            if (editContent.innerText == "+") {
+                editContent.innerText = "Save";
+                textBox.removeAttribute("readonly");
+                textBox.focus();
+            } else {
+                editContent.innerText = "+";
+                textBox.setAttribute("readonly", "readonly");
+            }
+        })
 
-            const textBox = document.createElement('input');
-            textBox.classList.add('text');
-            textBox.id = numTasks;
-            textBox.type = "text";
-            textBox.value = name;
-            textBox.setAttribute('readonly', 'readonly');
-
-            const editContent = document.createElement('button');
-            editContent.classList.add('edit');
-            editContent.innerText = '+';
-
-            const delContent = document.createElement('button');
-            delContent.classList.add('delete');
-            delContent.innerText = '-';
-
-            
-            taskContent.append(textBox);
-            taskContent.append(editContent);
-            taskContent.append(delContent);
-
-            tasklist.appendChild(taskContent);
-            
-            numTasks++;
-            
-            editContent.addEventListener('click', (event) =>{
-                if (editContent.innerText == "+") {
-                    editContent.innerText = "Save";
-                    textBox.removeAttribute("readonly");
-                    textBox.focus();
-                } else {
-                    editContent.innerText = "+";
-                    textBox.setAttribute("readonly", "readonly");
-                }
-            })
-
-            delContent.addEventListener('click', (event) => {
-                tasklist.removeChild(taskContent);
-            });
-        }
-    });
+        delContent.addEventListener('click', (event) => {
+            tasklist.removeChild(taskContent);
+        });
+    }
 });
