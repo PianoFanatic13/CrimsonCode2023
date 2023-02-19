4db6fc84f28959cea26b94fb1028a4d0346f0eb9
Peter Liu <124539148+liu-petter@users.noreply.github.com>
2023-02-19 02:04:47 -0800

    updated notecards

--- notecard.js
+++ notecard.js
@@ -1,23 +1,40 @@
 const inputCard = document.querySelector("#addTerm");
 const saveButton = document.querySelector("#saveTerm");
+const prevButton = document.querySelector("#prevCard");
+const nextButton = document.querySelector("#nextCard");
 
-let cardTerm = inputTerm.value;
+let term = inputCard.value
 let userIndex = 0;
 let noteCards = [];
 
-
-document.querySelector("#displayNoteCard").addEventListener("submit", (event) => {
+prevButton.addEventListener('click', (event) =>{
     event.preventDefault();
-    
-    saveButton.addEventListener('click', (event) =>{
-        alert("button clicked");
+    userIndex -= 1;
+    alert(userIndex);
+});
+
+nextButton.addEventListener('click', (event) =>{
+    event.preventDefault();
+    userIndex++;
+    alert(userIndex);
+});
+
+saveButton.addEventListener('click', (event) =>{
+    event.preventDefault();
+    if (!term){
         if (saveButton.innerText == "📝") {
             saveButton.innerText = "💾";
+            inputCard.setAttribute("value", term);
+            noteCards[userIndex] = [term];
             inputCard.removeAttribute("readonly");
             inputCard.focus();
         } else {
             saveButton.innerText = "📝";
             inputCard.setAttribute("readonly", "readonly");
         }
-    });
+    } else {
+        alert("Enter a term!");
+    }
 });
+
+
