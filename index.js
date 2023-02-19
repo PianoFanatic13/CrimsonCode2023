e9afd403f765ea219d2b3b847b3cb884281240da
Peter Liu <124539148+liu-petter@users.noreply.github.com>
2023-02-19 01:17:00 -0800

    included timer
    needs formatting

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const tasklist = document.querySelector("#tasks");
+const tasklist = document.querySelector("#tasks")
 const inputName = document.querySelector("#newTask-Name");
 let numTasks = 0;
 
@@ -46,10 +46,60 @@ document.querySelector("#newTask-Form").addEventListener("submit", (event) => {
                 editContent.innerText = "📝";
                 textBox.setAttribute("readonly", "readonly");
             }
-        })
+        });
 
         delContent.addEventListener('click', (event) => {
             tasklist.removeChild(taskContent);
         });
     }
 });
+// timer
+var start = document.getElementById('start');
+var reset = document.getElementById('reset');
+
+var hour = document.getElementById("hour");
+var minute = document.getElementById("minute");
+var second = document.getElementById("sec");
+
+let startTimer = undefined;
+
+function timer(){
+    if(hour.value == 0 && minute.value == 0 && second.value == 0){
+        hour.value = 0;
+        minute.value = 0;
+        second.value = 0;
+    } else if(second.value != 0){
+        second.value--;
+    } else if(minute.value != 0 && second.value == 0){
+        second.value = 59;
+        minute.value--;
+    } else if(hour.value != 0 && minute.value == 0){
+        minute.value = 60;
+        hour.value--;
+    }
+    return;
+}
+
+//function for when timer is reset, it stops the interval
+function stopInterval() {
+    clearInterval(startTimer);
+}
+
+
+start.addEventListener('click', function(){
+    function startInterval(){
+        startTimer = setInterval(function() {
+            timer();
+        }, 1000);
+    }
+    startInterval();
+})
+
+reset.addEventListener('click', function(){
+    hour.value = 0;
+    minute.value = 0;
+    second.value = 0;
+    //calls the function for when reset button is called
+    stopInterval()
+})
+
--- notecard.js
+++ notecard.js
@@ -1,4 +1,5 @@
-const notecard = document.querySelector("#displayNoteCard");
+const inputCard = document.querySelector("#addTerm");
+const saveButton = document.querySelector("#saveTerm");
 
 let cardTerm = inputTerm.value;
 let userIndex = 0;
@@ -8,4 +9,15 @@ let noteCards = [];
 document.querySelector("#displayNoteCard").addEventListener("submit", (event) => {
     event.preventDefault();
     
+    saveButton.addEventListener('click', (event) =>{
+        alert("button clicked");
+        if (saveButton.innerText == "📝") {
+            saveButton.innerText = "💾";
+            inputCard.removeAttribute("readonly");
+            inputCard.focus();
+        } else {
+            saveButton.innerText = "📝";
+            inputCard.setAttribute("readonly", "readonly");
+        }
+    });
 });
