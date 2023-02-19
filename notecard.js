697adcc16e1546b7b1395476cc722f830e727fe8
Peter Liu <124539148+liu-petter@users.noreply.github.com>
2023-02-19 07:26:56 -0800

    idk

--- notecard.js
+++ notecard.js
@@ -1,81 +1,85 @@
 const inputCard = document.querySelector("#addTerm");
-const saveButton = document.querySelector("#saveTerm");
 const prevButton = document.querySelector("#prevCard");
 const nextButton = document.querySelector("#nextCard");
 const flipCard = document.querySelector("#flipCard");
 const delCard = document.querySelector("#delCard");
+const newTerm = document.querySelector("#newTerm");
+const newAnsw = document.querySelector("#newAnswer");
 
 document.getElementById("cardSide").innerText = "Terms/Questions";
 
-console.log(inputCard);
-
 let userIndex = 0;
-let noteCard = [];
+let cardContent = [];
+if (JSON.parse(localStorage.getItem(userIndex))){
+    cardContent = JSON.parse(localStorage.getItem(userIndex));
+}
 
 // to keep track of card side in flipCard
 let num = 0;
+document.querySelector("#newCard").addEventListener("submit", (event) => {
 
+    event.preventDefault();
+    cardContent[0] = newTerm.value;
+    cardContent[1] = newAnsw.value;
+    document.getElementById("userIndex").value = userIndex;
+    localStorage.setItem(userIndex, JSON.stringify(cardContent));
+});
 
 prevButton.addEventListener('click', (event) =>{
     event.preventDefault();
     userIndex -= 1;
-    inputCard.value = localStorage.getItem(userIndex);
+    document.getElementById("userIndex").value = userIndex;
+    cardContent = JSON.parse(localStorage.getItem(userIndex));
+    if (sideState(num) == "term"){
+        inputCard.value = cardContent[0];
+    } else if (sideState(num) == "answer"){
+        inputCard.value = cardContent[1];
+    }
+    num++;
     console.log(userIndex);
-
 });
 
 nextButton.addEventListener('click', (event) =>{
     event.preventDefault();
     userIndex += 1;
-    inputCard.value = localStorage.getItem(userIndex);
+    document.getElementById("userIndex").value = userIndex;
     console.log(userIndex);
-    
+    if (sideState(num) == "term"){
+        inputCard.value = cardContent[0];
+    } else if (sideState(num) == "answer"){
+        inputCard.value = cardContent[1];
+    }
+    num++;
 });
+
+function sideState(num){
+    if(num % 2 == 1){
+        return "term";
+    } else {
+        return "answer";
+    }
+}
 
 flipCard.addEventListener('click', (event) =>{
     event.preventDefault();
-
+    
     let side = "Terms/Questions";
-    if(num % 2 == 1){
-        side = "Terms/Questions";
-        inputCard.placeholder="Enter a term/question";
-        localStorage.setItem(userIndex, JSON.stringify([null,inputCard.term]));
-    } else {
-        side = "Answer";
-        inputCard.placeholder="Enter the answer";
-        localStorage.setItem(userIndex, JSON.stringify([inputCard.term,null]));
 
+    if (sideState(num) == "term"){
+        side = "Terms/Questions";
+    } else if (sideState(num) == "answer"){
+        side = "Answer";
     }
     num++;
-    console.log(noteCard);
+    
     document.getElementById("cardSide").innerText = side;
-    inputCard.value = null;
 });
 
 delCard.addEventListener('click', (event) =>{
     event.preventDefault();
+    localStorage.removeItem(userIndex);
 
 });
 
-saveButton.addEventListener('click', (event) =>{
-    event.preventDefault();
-    let term = inputCard.value;
-    let noteCard = [];
-    
-    if (saveButton.innerText == "📝") {
-        saveButton.innerText = "💾";
-        
-        localStorage.setItem(userIndex,  JSON.stringify(noteCard));
- 
-        console.log(localStorage.getItem(userIndex));
-        noteCard = JSON.parse(localStorage.getItem(userIndex));
 
-        inputCard.removeAttribute("readonly");
-        inputCard.focus();
-    } else {
-        saveButton.innerText = "📝";
-        inputCard.setAttribute("readonly", "readonly");
-    }
-
-});
 
