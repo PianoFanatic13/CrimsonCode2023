fb0dfa0059084af22d5f1351cdd5d7579a4d7609
Peter Liu <124539148+liu-petter@users.noreply.github.com>
2023-02-19 03:56:29 -0800

    update to format and functionality of notecards

--- notecard.js
+++ notecard.js
@@ -2,14 +2,17 @@ const inputCard = document.querySelector("#addTerm");
 const saveButton = document.querySelector("#saveTerm");
 const prevButton = document.querySelector("#prevCard");
 const nextButton = document.querySelector("#nextCard");
+const flipCard = document.querySelector("#flipCard");
 
-let term = inputCard.value;
+console.log(inputCard);
+
 let userIndex = 0;
-let noteCards = [];
+let noteCard = [];
 
 prevButton.addEventListener('click', (event) =>{
     event.preventDefault();
     userIndex -= 1;
+    inputCard.value = localStorage.getItem(userIndex);
     console.log(userIndex);
 
 });
@@ -17,25 +20,38 @@ prevButton.addEventListener('click', (event) =>{
 nextButton.addEventListener('click', (event) =>{
     event.preventDefault();
     userIndex += 1;
+    inputCard.value = localStorage.getItem(userIndex);
     console.log(userIndex);
     
 });
 
+flipCard.addEventListener('click', (event) =>{
+    event.preventDefault();
+    inputCard.value = 'hello';
+});
+
 saveButton.addEventListener('click', (event) =>{
     event.preventDefault();
+    let term = inputCard.value;
+    
     if (!term){
+        alert("Enter a term!");
+    } else {
+        
         if (saveButton.innerText == "📝") {
             saveButton.innerText = "💾";
-            window.localStorage.setItem(userIndex, term);
-            console.log(window.localStorage.getItem(userIndex));
+
+            document.getElementById("userIndex").innerText = userIndex;
+
+            localStorage.setItem(userIndex, term);
+            console.log(localStorage.getItem(userIndex));
+
             inputCard.removeAttribute("readonly");
             inputCard.focus();
         } else {
             saveButton.innerText = "📝";
             inputCard.setAttribute("readonly", "readonly");
         }
-    } else {
-        alert("Enter a term!");
     }
 });
 
