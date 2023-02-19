9517af52af2203d8080407cec6623a36a1312256
Peter Liu <124539148+liu-petter@users.noreply.github.com>
2023-02-19 05:42:34 -0800

    idk what i did

--- notecard.js
+++ notecard.js
@@ -3,6 +3,7 @@ const saveButton = document.querySelector("#saveTerm");
 const prevButton = document.querySelector("#prevCard");
 const nextButton = document.querySelector("#nextCard");
 const flipCard = document.querySelector("#flipCard");
+const delCard = document.querySelector("#delCard");
 
 document.getElementById("cardSide").innerText = "Terms/Questions";
 
@@ -18,7 +19,6 @@ let num = 0;
 prevButton.addEventListener('click', (event) =>{
     event.preventDefault();
     userIndex -= 1;
-    document.getElementById("coutIndex").innerText = userIndex;
     inputCard.value = localStorage.getItem(userIndex);
     console.log(userIndex);
 
@@ -27,7 +27,6 @@ prevButton.addEventListener('click', (event) =>{
 nextButton.addEventListener('click', (event) =>{
     event.preventDefault();
     userIndex += 1;
-    document.getElementById("coutIndex").innerText = userIndex;
     inputCard.value = localStorage.getItem(userIndex);
     console.log(userIndex);
     
@@ -39,23 +38,37 @@ flipCard.addEventListener('click', (event) =>{
     let side = "Terms/Questions";
     if(num % 2 == 1){
         side = "Terms/Questions";
+        inputCard.placeholder="Enter a term/question";
+        localStorage.setItem(userIndex, JSON.stringify([null,inputCard.term]));
     } else {
         side = "Answer";
+        inputCard.placeholder="Enter the answer";
+        localStorage.setItem(userIndex, JSON.stringify([inputCard.term,null]));
+
     }
     num++;
+    console.log(noteCard);
     document.getElementById("cardSide").innerText = side;
     inputCard.value = null;
+});
+
+delCard.addEventListener('click', (event) =>{
+    event.preventDefault();
+
 });
 
 saveButton.addEventListener('click', (event) =>{
     event.preventDefault();
     let term = inputCard.value;
+    let noteCard = [];
     
     if (saveButton.innerText == "📝") {
         saveButton.innerText = "💾";
-
-        localStorage.setItem(userIndex, term);
+        
+        localStorage.setItem(userIndex,  JSON.stringify(noteCard));
+ 
         console.log(localStorage.getItem(userIndex));
+        noteCard = JSON.parse(localStorage.getItem(userIndex));
 
         inputCard.removeAttribute("readonly");
         inputCard.focus();
