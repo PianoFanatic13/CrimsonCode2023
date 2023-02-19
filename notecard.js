f52424c985e82e48c317cba5e0195a1a975c4e25
Peter Liu <124539148+liu-petter@users.noreply.github.com>
2023-02-19 00:20:25 -0800

    restart on notecards

--- notecard.js
+++ notecard.js
@@ -1,36 +1,11 @@
 const notecard = document.querySelector("#displayNoteCard");
 
+let cardTerm = inputTerm.value;
 let userIndex = 0;
 let noteCards = [];
-
-function newCard(){
-
-    const cardElements = document.createElement('div');
-    const query = document.createElement('input');
-    query.classList.add('text');
-    query.id = "newTerm";
-    query.type = "text";
-    query.placeholder = "Enter a term/question";
-
-    const saveTerm = document.createElement('button');
-    saveTerm.classList.add("edit");
-    saveTerm.innerText = "💾";
-
-    cardElements.append(query); 
-    cardElements.append(saveTerm);          
-    notecard.appendChild(cardElements);
-}   
 
 
 document.querySelector("#displayNoteCard").addEventListener("submit", (event) => {
     event.preventDefault();
-
-    if (noteCards[userIndex] = null){
-        newCard();
-    }
-    else {
-        
-    }
     
-
 });
